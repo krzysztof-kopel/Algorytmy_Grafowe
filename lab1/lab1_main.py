905bd42af34962dcd35b853a4399d6deecf13d4d
krzysbielsko <krzysbielsko@gmail.com>
2024-10-27 22:11:52 +0100

    Zmiana sposobu wprowadzania grafu do testu.

--- lab1/lab1_main.py
+++ lab1/lab1_main.py
@@ -5,4 +5,8 @@ file_name = input("Podaj nazwę pliku z grafem: ")
 vertex_number, edges = load_graph(file_name)
 graph = to_adjacency_list(vertex_number, edges)
 
+with open(f"..\\example_graphs\\{file_name}") as file:
+    correct_answer = int(file.readline().split()[-1])
+print(f"Wynik prawidłowy: {correct_answer}")
+
 print(f"Wynik algorytmu Dijkstry: {dijkstra(graph)}")
