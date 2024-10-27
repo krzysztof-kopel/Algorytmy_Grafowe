e47b80c9dda194618a42dd390f3b565c873d3235
krzysbielsko <krzysbielsko@gmail.com>
2024-10-27 22:08:18 +0100

    Zmiana sposobu wyboru grafu do testu.

--- lab1/lab1_main.py
+++ lab1/lab1_main.py
@@ -1,7 +1,8 @@
 from graph_input import load_graph, to_adjacency_list
 from dijkstra import dijkstra
 
-vertex_number, edges = load_graph("clique1000")
+file_name = input("Podaj nazwę pliku z grafem: ")
+vertex_number, edges = load_graph(file_name)
 graph = to_adjacency_list(vertex_number, edges)
 
 print(f"Wynik algorytmu Dijkstry: {dijkstra(graph)}")
