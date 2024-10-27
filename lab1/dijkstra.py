4cb302345d1291637dd5da49e64e03efb8e62560
krzysbielsko <krzysbielsko@gmail.com>
2024-10-27 22:07:16 +0100

    Usprawnienie algorytmu Dijkstry - usunięcie redundantnej tablicy parents.

--- lab1/dijkstra.py
+++ lab1/dijkstra.py
@@ -4,24 +4,20 @@ from queue import PriorityQueue
 def dijkstra(graph, start=0, end=1):
     # graph = adj_list
     queue = PriorityQueue()
-    queue.put((-float('inf'), start))
+    queue.put((-float('inf'), start, float('inf')))
     results = [-float('inf')] * len(graph)
     results[start] = float('inf')
-    parents = [(None, None)] * len(graph)
+    final_result = float('inf')
 
     while not queue.empty():
-        prev_min, vertex = queue.get()
+        prev_min, vertex, min_on_path = queue.get()
         prev_min *= -1
         for i in range(len(graph[vertex])):
             new_vertex, new_weight = graph[vertex][i]
             if min(new_weight, prev_min) > results[new_vertex]:
                 results[new_vertex] = min(new_weight, prev_min)
-                parents[new_vertex] = (vertex, new_weight)
-                queue.put((-min(new_weight, prev_min), new_vertex))
+                if new_vertex == end:
+                    final_result = min(new_weight, min_on_path)
+                queue.put((-min(new_weight, prev_min), new_vertex, min(min_on_path, new_weight)))
 
-    result = float('inf')
-    i = end
-    while i != start:
-        result = min(result, parents[i][1])
-        i = parents[i][0]
-    return result
+    return final_result
--- lab1/lab1_main.py
+++ lab1/lab1_main.py
@@ -1,7 +1,7 @@
 from graph_input import load_graph, to_adjacency_list
 from dijkstra import dijkstra
 
-vertex_number, edges = load_graph("clique100")
+vertex_number, edges = load_graph("clique1000")
 graph = to_adjacency_list(vertex_number, edges)
 
 print(f"Wynik algorytmu Dijkstry: {dijkstra(graph)}")
