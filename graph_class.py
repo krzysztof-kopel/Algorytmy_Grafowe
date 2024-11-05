60ac7c3076dc870fc45ae1797365d7aa77098aa3
KK04 <72666836+krzysztof-kopel@users.noreply.github.com>
2024-11-05 12:52:17 +0100

    Klasa do przechowywania grafu.

---
+++ graph_class.py
@@ -0,0 +1,24 @@
+class Node:
+    def __init__(self):
+        self.edges = dict()
+
+    def add_edge(self, to, weight):
+        self.edges[to] = self.edges.get(to, 0) + weight
+
+    def del_edge(self, to):
+        del self.edges[to]
+
+
+class Graph:
+    def __init__(self, adj_list):
+        self.adj_list = adj_list
+        self.nodes = []
+        self.size = 0
+        for i in range(len(adj_list)):
+            curr_node = Node()
+            for j in range(adj_list[i]):
+                curr_node.add_edge(adj_list[i][j][0], adj_list[i][j][1])
+                self.size += 1
+            self.nodes.append(curr_node)
+
+        self.rank = len(adj_list)
