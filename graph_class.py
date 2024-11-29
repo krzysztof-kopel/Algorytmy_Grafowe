80e1e23f7050c415a69401f81f5b3c7d6f97c597
KK04 <72666836+krzysztof-kopel@users.noreply.github.com>
2024-11-29 11:09:46 +0100

    Dalszy rozwój klasy Graph - dodanie add_edge i merge_vertex, jak również testów dla klasy.

--- graph_class.py
+++ graph_class.py
@@ -1,9 +1,11 @@
-class Node:
-    def __init__(self):
-        self.edges = dict()
+from graph_input import load_graph
 
-    def add_edge(self, to, weight):
-        self.edges[to] = self.edges.get(to, 0) + weight
+
+class Node:
+    def __init__(self, number):
+        self.number = number
+        self.edges = dict()
+        self.inactive = False
 
     def del_edge(self, to):
         del self.edges[to]
@@ -15,10 +17,52 @@ class Graph:
         self.nodes = []
         self.size = 0
         for i in range(len(adj_list)):
-            curr_node = Node()
-            for j in range(adj_list[i]):
-                curr_node.add_edge(adj_list[i][j][0], adj_list[i][j][1])
-                self.size += 1
-            self.nodes.append(curr_node)
+            new_node = Node(i)
+            self.nodes.append(new_node)
+        for i in range(len(adj_list)):
+            for j in range(len(adj_list[i])):
+                self.add_edge(i, adj_list[i][j][0], adj_list[i][j][1], True)
 
         self.rank = len(adj_list)
+
+    def display_graph(self):
+        print(f"Rozmiar: {int(self.size)}\nRząd: {self.rank}")
+        for node in self.nodes:
+            print(f"Wierzchołek numer {node.number}: ", end="")
+            for neighbour in node.edges.items():
+                print(f"{neighbour[0]}({neighbour[1]}) ", end=" ")
+            if node.inactive:
+                print("(Nieaktywny)", end="")
+            print()
+        print()
+
+
+    def merge_vertices(self, x, y):
+        self.nodes[x].del_edge(y)
+        vertex_y = self.nodes[y]
+        for edge in list(vertex_y.edges.items()):
+            if edge[0] != x:
+                self.add_edge(x, edge[0], edge[1], False)
+                self.nodes[edge[0]].del_edge(y)
+            vertex_y.del_edge(edge[0])
+            self.size -= 1
+        vertex_y.inactive = True
+
+    def add_edge(self, start, to, weight, directed):
+        start_node = self.nodes[start]
+        to_node = self.nodes[to]
+        start_node.edges[to] = start_node.edges.get(to, 0) + weight
+        if not directed:
+            to_node.edges[start] = to_node.edges.get(start, 0) + weight
+        self.size += 1
+
+
+if __name__ == "__main__":
+    from graph_input import to_adjacency_list, load_graph
+    adj_list = to_adjacency_list(*load_graph("path", "grafowe\\lab3\\graphs-lab3"))
+    graph = Graph(adj_list)
+    graph.display_graph()
+    graph.add_edge(0,5, 4, False)
+    graph.display_graph()
+    graph.merge_vertices(0, 5)
+    graph.display_graph()
