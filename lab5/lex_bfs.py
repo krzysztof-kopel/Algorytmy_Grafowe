f78bec94be6ea7f5702e190f6107d779f582df89
KK04 <72666836+krzysztof-kopel@users.noreply.github.com>
2024-12-16 16:55:39 +0100

    Ostateczna wersja (najprostszego) lex-BFS

--- lab5/lex_bfs.py
+++ lab5/lex_bfs.py
@@ -8,7 +8,7 @@ def partition_set(node_set: set[int], vertex: Node):
     foreign_set = set()
     while len(node_set) != 0:
         next_node = node_set.pop()
-        if vertex.connect_to(next_node):
+        if next_node in vertex.out:
             neighbor_set.add(next_node)
         else:
             foreign_set.add(next_node)
@@ -46,18 +46,15 @@ def lex_bfs(graph: list[Node], start_index=0) -> list[Node]:
 
 def check_lex_bfs(graph, vs):
     n = len(graph)
-    # pi[v] stores the position of vertex v in the Lex-BFS order.
     pi = [None] * n
     for i, v in enumerate(vs):
         pi[v] = i
 
-    # Check Lex-BFS condition for every pair of vertices
     for i in range(n - 1):
-        ni = graph[vs[i]].out  # Neighbors of vs[i]
+        ni = graph[vs[i]].out
         for j in range(i + 1, n):
-            nj = graph[vs[j]].out  # Neighbors of vs[j]
+            nj = graph[vs[j]].out
 
-            # Calculate vertices violating Lex-BFS ordering
             conflicting = [pi[v] for v in nj - ni if pi[v] < i]
             if conflicting:
                 viable = [pi[v] for v in ni - nj]
@@ -68,12 +65,12 @@ def check_lex_bfs(graph, vs):
 
 
 if __name__ == "__main__":
-    rank, edges = load_graph("franklin", "lab5\\graphs-lab5\\chordal")
+    graph_name = input("Podaj nazwę pliku z grafem: ")
+    rank, edges = load_graph(graph_name, "lab5\\graphs-lab5\\chordal")
     graph = [Node(i) for i in range(rank)]
     for (u, v, _) in edges:
         graph[u - 1].connect_to(v - 1)
         graph[v - 1].connect_to(u - 1)
     lex_bfs_ordering = lex_bfs(graph)
+    print(lex_bfs_ordering)
     print(check_lex_bfs(graph, lex_bfs_ordering))
-
-
