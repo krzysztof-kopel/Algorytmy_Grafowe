cf762be5f4917c7d4db31b679f74cc2c1a5aa949
KK04 <72666836+krzysztof-kopel@users.noreply.github.com>
2024-12-16 16:00:29 +0100

    Pierwsza wersja Lex_bfs

---
+++ lab5/lex_bfs.py
@@ -0,0 +1,49 @@
+from node_class import Node
+from graph_input import load_graph
+
+def partition_set(node_set: set[int], vertex: Node):
+    # Funkcja dzieli node_set na set tych wierzchołków, które nie sąsiadują z vertex i set tych, które sąsiadują, po
+    # czym zwraca je w tej kolejności.
+    neighbor_set = set()
+    foreign_set = set()
+    while len(node_set) != 0:
+        next_node = node_set.pop()
+        if vertex.connect_to(next_node):
+            neighbor_set.add(next_node)
+        else:
+            foreign_set.add(next_node)
+    return foreign_set, neighbor_set
+
+
+def lex_bfs(graph: list[Node], start_index=0) -> list[Node]:
+    # Wersja o najgorszej złożoności obliczeniowej
+
+    # node_sets -> lista zbiorów, gdzie każdy zbiór zawiera wierzchołki mające takie same (leksykograficznie) zbiory poprzedników
+    result_ordering = []
+    node_sets = [set(), {start_index}]
+    for node in graph:
+        if node.idx != start_index:
+            node_sets[0].add(node.idx)
+
+    while len(node_sets) != 0:
+        current_node = node_sets[-1].pop()
+        result_ordering.append(current_node)
+        new_node_sets_list = []
+        if len(node_sets[-1]) == 0:
+            node_sets.pop()
+
+        for i in range(len(node_sets)):
+            set_1, set_2 = partition_set(node_sets[i], current_node)
+            if len(set_1) != 0:
+                new_node_sets_list.append(set_1)
+            if len(set_2) != 0:
+                new_node_sets_list.append(set_2)
+
+        node_sets = new_node_sets_list
+
+    return result_ordering
+
+
+if __name__ == "__main__":
+    rank, edges = load_graph("clique4", "lab5\\graphs-lab5\\chordal")
+    # TODO zmienić indeksowanie na zaczynające się od 0 i dalej testować funkcję
