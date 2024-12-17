325307f32ef10f499df8f3aaa8c73f08b3d784a8
KK04 <72666836+krzysztof-kopel@users.noreply.github.com>
2024-12-17 01:00:57 +0100

    Poprawki w algorytmie Prima i jego tester.

--- projekt_1/example.py
+++ projekt_1/example.py
@@ -1,5 +1,5 @@
 from queue import PriorityQueue
-from data import runtests
+# from data import runtests
 
 def to_adjacency_list(vertex_number, edges):
     adj_list = [[] for _ in range(vertex_number)]
@@ -12,7 +12,7 @@ def to_adjacency_list(vertex_number, edges):
 
     return adj_list
 
-def prim(graph: list[list[int]], start_index: int=0) -> set[tuple[int, int]]:
+def prim(graph: list[list[int]], start_index: int=0, both_sides: bool=True) -> set[tuple[int, int]]:
     # queue: (weight, edge_to, edge_from)
     queue = PriorityQueue()
     queue.put((0, start_index, None))
@@ -25,7 +25,10 @@ def prim(graph: list[list[int]], start_index: int=0) -> set[tuple[int, int]]:
             continue
         visited.add(edge_to)
 
-        edges_in_mst.add((edge_from, edge_to))
+        if edge_from is not None:
+            edges_in_mst.add((edge_from, edge_to))
+            if both_sides:
+                edges_in_mst.add((edge_to, edge_from))
         for destination, weight in graph[edge_to]:
             if destination not in visited:
                 queue.put((weight, destination, edge_to))
@@ -38,4 +41,4 @@ def my_solve(N, streets, lords):
     graph = to_adjacency_list(N, streets)
     return list(prim(graph))
 
-runtests(my_solve)
+# runtests(my_solve)
