0f7fa14ea75e539e3e371f3d64ddc3418daec1cb
KK04 <72666836+krzysztof-kopel@users.noreply.github.com>
2025-01-20 17:31:59 +0100

    Max-clique algorithm implemented.

--- lab5/algorithms.py
+++ lab5/algorithms.py
@@ -14,3 +14,15 @@ def is_chordal(graph: list[Node]) -> bool:
             return False
         predecessors[lex_bfs_ordering[i].idx] = set(neighbors)
     return True
+
+def max_clique(graph: list[Node]) -> int:
+    lex_bfs_ordering = lex_bfs_nodes(graph)
+    max_clique_size = 0
+    for i in range(1, len(lex_bfs_ordering)):
+        vertex = lex_bfs_ordering[i]
+        neighbors = list()
+        for j in range(i):
+            if lex_bfs_ordering[j].idx in vertex.out:
+                neighbors.append(lex_bfs_ordering[j].idx)
+        max_clique_size = max(max_clique_size, len(neighbors) + 1)
+    return max_clique_size
--- lab5/lab5_main.py
+++ lab5/lab5_main.py
@@ -1,6 +1,6 @@
 from algorithm_comparator import compare_algorithms
 from graph_input import load_graph
-from lab5.chordal import is_chordal
+from lab5.algorithms import is_chordal, max_clique
 from node_class import Node
 
 folder_name = input("Podaj nazwę folderu, z którego chcesz wziąć graf (chordal, coloring, interval, maxclique, vcover): ")
@@ -9,11 +9,11 @@ file_name = input("Podaj nazwę pliku z grafem: ")
 
 with open(f"graphs-lab5\\{folder_name}\\{file_name}") as file:
     answer = int(file.readline().split()[-1])
-print(f"Wynik prawidłowy: {bool(answer)}")
+print(f"Wynik prawidłowy {folder_name}: {answer}")
 
 rank, edges = load_graph(file_name, f"lab5\\graphs-lab5\\{folder_name}")
 graph = [Node(i) for i in range(rank)]
 for (u, v, _) in edges:
     graph[u - 1].connect_to(v - 1)
     graph[v - 1].connect_to(u - 1)
-compare_algorithms([is_chordal], ["sprawdzania, czy graf jest przekątniowy"], graph)
+compare_algorithms([is_chordal, max_clique], ["sprawdzania, czy graf jest przekątniowy", "wyznacza rozmiaru największej kliki"], graph)
